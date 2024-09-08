9c292050d8308e3f62dca823ffe60352612f80ab
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-09-08 10:22:24 +0900

    環境によって異なるcontainerを使うように修正

--- lib/cdk-race-schedule-app-stack.ts
+++ lib/cdk-race-schedule-app-stack.ts
@@ -1,18 +1,61 @@
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dotenv from 'dotenv';
 import {
   aws_lambda_nodejs,
   aws_apigateway,
+  aws_s3,
   Stack,
   Duration,
   CfnOutput,
   StackProps,
 } from 'aws-cdk-lib';
+import {
+  Effect,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+
+if (process.env.GITHUB_ACTIONS !== 'true') { dotenv.config({ path: './app.env' }); }
 
 export class CdkRaceScheduleAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // S3バケットの参照
+    const bucket = aws_s3.Bucket.fromBucketName(
+      this,
+      'RaceScheduleBucket',
+      'race-schedule-bucket',
+    );
+
+    // Lambda実行に必要なIAMロールを作成
+    const role = new Role(this, "LambdaExecutionRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    // Lambda が S3 バケットにアクセスできるようにするポリシーステートメントを追加
+    role.addToPolicy(
+      new PolicyStatement({
+        actions: ['s3:GetObject', 's3:PutObject'],
+        resources: [bucket.bucketArn + '/*'],
+      })
+    );
+
+    // Lambdaのログ出力権限を追加
+    role.addToPolicy(
+      new PolicyStatement({
+        actions: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        effect: Effect.ALLOW,
+        resources: ["*"],
+      })
+    );
 
     // Lambda関数を作成
     const lambdaFunction = new aws_lambda_nodejs.NodejsFunction(
@@ -22,10 +65,16 @@ export class CdkRaceScheduleAppStack extends Stack {
         architecture: lambda.Architecture.ARM_64,
         runtime: lambda.Runtime.NODEJS_20_X,
         entry: 'lib/src/index.ts',
+        role: role,
         environment: {
-          NODE_ENV: 'production',
+          NODE_ENV: process.env.NODE_ENV || 'local',
+          NAR_CALENDAR_ID: process.env.NAR_CALENDAR_ID || '',
+          GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL || '',
+          // GOOGLE_PRIVATE_KEY は改行コードを含むため、置換が必要
+          GOOGLE_PRIVATE_KEY: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
         },
-      }
+        timeout: Duration.seconds(30),
+      },
     );
 
     // API Gatewayの設定
@@ -50,9 +99,7 @@ export class CdkRaceScheduleAppStack extends Stack {
       },
     });
 
-    /**
-     * API Gateway エンドポイントの出力
-     */
+    // API Gateway エンドポイントの出力
     new CfnOutput(this, 'RaceScheduleAppApiGatewayEndpoint', {
       value: api.deploymentStage.urlForPath(),
     });
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,17 +2,19 @@ import "reflect-metadata";
 import express from "express";
 import serverlessExpress from '@codegenie/serverless-express';
 import { container } from "tsyringe";
-import { NarRaceData } from "./domain/narRaceData";
-import { ICalendarService } from "./service/interface/ICalendarService";
-import { IRaceRepository } from "./repository/interface/IRaceRepository";
-import { NarPlaceData } from "./domain/narPlaceData";
 import { NarRaceController } from "./controller/narRaceController";
-import { IRaceCalendarUseCase } from "./usecase/interface/IRaceCalendarUseCase";
-import { NarRaceCalendarUseCase } from "./usecase/implement/narRaceCalendarUseCase";
-import { MockGoogleCalendarService } from "./service/mock/mockGoogleCalendarService";
-import { NarRaceRepositoryFromS3Impl } from "./repository/implement/narRaceRepositoryFromS3Impl";
+import { NarPlaceData } from "./domain/narPlaceData";
+import { NarRaceData } from "./domain/narRaceData";
+import { S3Gateway } from "./gateway/implement/s3Gateway";
 import { IS3Gateway } from "./gateway/interface/iS3Gateway";
 import { MockS3Gateway } from "./gateway/mock/mockS3Gateway";
+import { NarRaceRepositoryFromS3Impl } from "./repository/implement/narRaceRepositoryFromS3Impl";
+import { IRaceRepository } from "./repository/interface/IRaceRepository";
+import { GoogleCalendarService } from "./service/implement/googleCalendarService";
+import { ICalendarService } from "./service/interface/ICalendarService";
+import { MockGoogleCalendarService } from "./service/mock/mockGoogleCalendarService";
+import { NarRaceCalendarUseCase } from "./usecase/implement/narRaceCalendarUseCase";
+import { IRaceCalendarUseCase } from "./usecase/interface/IRaceCalendarUseCase";
 
 // Expressアプリケーションの設定
 const app = express();
@@ -20,14 +22,42 @@ const app = express();
 // DIコンテナの初期化
 // s3Gatewayの実装クラスをDIコンテナに登錄する
 container.register<IS3Gateway<NarRaceData>>(
-    'IS3Gateway',
-    { useFactory: () => new MockS3Gateway<NarRaceData>('race-schedule-bucket', 'nar/race/') }
+    'IS3GatewayForNarRace',
+    {
+        useFactory: () => {
+            switch (process.env.NODE_ENV) {
+                case 'production':
+                    // NODE_ENV が production の場合、S3Gateway を使用
+                    return new S3Gateway<NarRaceData>('race-schedule-bucket', 'nar/race/');
+                case 'local':
+                    // NODE_ENV が local の場合、MockS3Gateway を使用
+                    return new MockS3Gateway<NarRaceData>('race-schedule-bucket', 'nar/race/');
+                default:
+                    // NODE_ENV が ない場合、MockS3Gateway を使用
+                    return new MockS3Gateway<NarRaceData>('race-schedule-bucket', 'nar/race/');
+            }
+        }
+    }
 );
 
 // ICalendarServiceの実装クラスをDIコンテナに登錄する
 container.register<ICalendarService<NarRaceData>>(
     'ICalendarService',
-    { useClass: MockGoogleCalendarService }
+    {
+        useFactory: () => {
+            switch (process.env.NODE_ENV) {
+                case 'production':
+                    // NODE_ENV が production の場合、GoogleCalendarService を使用
+                    return new GoogleCalendarService<NarRaceData>('nar', process.env.NAR_CALENDAR_ID || '');
+                case 'local':
+                    // NODE_ENV が local の場合、MockGoogleCalendarService を使用
+                    return new MockGoogleCalendarService();
+                default:
+                    // NODE_ENV が指定されていない場合も MockGoogleCalendarService を使用
+                    return new MockGoogleCalendarService();
+            }
+        }
+    }
 );
 
 // Repositoryの実装クラスをDIコンテナに登錄する
