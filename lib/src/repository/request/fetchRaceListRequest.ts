f2f6a52a08b51df246dca7d862b8ccc3f8514842
Taichi.N <49602502+taichi6930@users.noreply.github.com>
2024-09-14 20:50:44 +0900

    テストの修正

--- lib/src/repository/request/fetchRaceListRequest.ts
+++ lib/src/repository/request/fetchRaceListRequest.ts
@@ -6,10 +6,5 @@ export class FetchRaceListRequest<P> {
         public readonly startDate: Date,
         public readonly endDate: Date,
         public readonly placeDataList?: P[],
-    ) {
-        // startDateがendDateよりも後の日付の場合はエラー
-        if (startDate > endDate) {
-            throw new Error('startDate must be earlier than endDate');
-        }
-    }
+    ) {}
 }
