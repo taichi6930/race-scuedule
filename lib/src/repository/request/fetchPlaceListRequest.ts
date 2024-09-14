861773d64807e7f77d163d295789ceeff5d150b2
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-09-14 20:48:48 +0900

    不要な判定を削除

--- lib/src/repository/request/fetchPlaceListRequest.ts
+++ lib/src/repository/request/fetchPlaceListRequest.ts
@@ -5,10 +5,5 @@ export class FetchPlaceListRequest {
     constructor(
         public readonly startDate: Date,
         public readonly endDate: Date,
-    ) {
-        // startDateがendDateよりも後の日付の場合はエラー
-        if (startDate > endDate) {
-            throw new Error('startDate must be earlier than endDate');
-        }
-    }
+    ) {}
 }
