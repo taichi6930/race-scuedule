00d530e647e9ea70c826812d863e940ef095a57f
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-09-14 18:13:28 +0900

    narPlaceRepositoryFromS3のテスト実装

--- test/src/mock/gateway/s3GatewayMock.ts
+++ test/src/mock/gateway/s3GatewayMock.ts
@@ -1,8 +1,18 @@
+import { NarPlaceData } from '../../../../lib/src/domain/narPlaceData';
 import { NarRaceData } from '../../../../lib/src/domain/narRaceData';
 import { IS3Gateway } from '../../../../lib/src/gateway/interface/iS3Gateway';
 
 export const mockS3GatewayForNarRace = (): jest.Mocked<
     IS3Gateway<NarRaceData>
+> => {
+    return {
+        uploadDataToS3: jest.fn(),
+        fetchDataFromS3: jest.fn(),
+    };
+};
+
+export const mockS3GatewayForNarPlace = (): jest.Mocked<
+    IS3Gateway<NarPlaceData>
 > => {
     return {
         uploadDataToS3: jest.fn(),
