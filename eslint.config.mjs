ea1f1b447d92637f5101f90976e4f0748db8c87d
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-09-15 11:37:21 +0900

    ルールを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 import tseslint from 'typescript-eslint';
 import unusedImports from 'eslint-plugin-unused-imports';
+import eslint from '@eslint/js';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    // lint対象ファイル
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+    },
     {
         languageOptions: {
             parserOptions: {
@@ -13,8 +17,9 @@ export default [
             },
         },
     },
-    ...tseslint.configs.strictTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
+    eslint.configs.recommended, // ESLintの推奨ルールを適用
+    ...tseslint.configs.strictTypeChecked, // TypeScriptの厳格な型チェックを適用
+    ...tseslint.configs.stylisticTypeChecked, // TypeScriptのスタイルチェックを適用
     eslintConfigPrettier, // Prettierとの競合を防ぐ設定
     {
         plugins: {
@@ -32,34 +37,8 @@ export default [
             '@typescript-eslint/restrict-plus-operands': 'off',
             '@typescript-eslint/no-unsafe-argument': 'off',
             '@typescript-eslint/no-misused-promises': 'off',
-
-            'array-callback-return': 'error', // 配列のコールバック関数にはreturn文を必ず記述する
-            'constructor-super': 'error', // constructor内でsuper()を呼び出す
-            'no-cond-assign': 'error', // 条件式で代入演算子を使用しない
-            'no-constant-condition': 'error', // typoによって条件文が必ず通る、または通らなくなるということを防ぐ
-            'no-dupe-else-if': 'error', // if-else-ifチェーンの重複条件の禁止
-            'no-duplicate-case': 'error', // ケースラベルの重複を禁止する
-            'no-empty-character-class': 'error', // 正規表現の空文字クラスを禁止する
-            'no-unused-vars': 'off', // DIコンテナで使われる変数を検出できないため無効化
-            'no-template-curly-in-string': 'error', // 文字列リテラル内のテンプレートリテラルを検出する
-            'no-useless-concat': 'error', // 文字列の連結を禁止する
-            'no-self-compare': 'error', // 自分自身との比較を禁止する
-            'unused-imports/no-unused-imports': 'error', // 使われていないimportを検出
-            'unused-imports/no-unused-vars': [
-                'error',
-                {
-                    vars: 'all',
-                    varsIgnorePattern: '^_',
-                    args: 'after-used',
-                    argsIgnorePattern: '^_',
-                },
-            ],
             // 他のルールを追加
-            'prettier/prettier': 'error', // Prettierのスタイルルールをエラーとして報告
-            'eqeqeq': 'error', // 厳密等価演算子(===)を必須にする
-            'curly': ['error', 'all'], // if文などで常に波括弧を使用する
-            'no-multiple-empty-lines': ['error', { max: 1 }], // 複数の空行を禁止
-            'no-trailing-spaces': 'error', // 行末の空白を禁止
+            'prettier/prettier': ['error', {}, { usePrettierrc: true }],
         },
     },
 ];
