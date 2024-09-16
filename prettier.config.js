4878ecea5a910507126672552eb5dcee4dbb36d6
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-09-16 13:28:35 +0900

    prettierの設定方法を修正

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    singleQuote: true,
+    tabWidth: 4,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    semi: true,
+    quoteProps: 'consistent',
+    trailingComma: 'all',
+    arrowParens: 'always',
+};
