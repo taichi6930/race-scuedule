449b6cb93e80756f0199036aa6d14e7610546959
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-09-16 11:41:01 +0900

    typescript-eslint/explicit-module-boundary-typeのrule追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,6 +40,7 @@ export default [
             '@typescript-eslint/no-unsafe-argument': 'off',
             '@typescript-eslint/no-misused-promises': 'off',
             '@typescript-eslint/consistent-type-imports': 'error',
+            '@typescript-eslint/explicit-module-boundary-types': 'error',
             // 他のルールを追加
             'prettier/prettier': ['error', {}, { usePrettierrc: true }],
             'simple-import-sort/imports': 'error',
