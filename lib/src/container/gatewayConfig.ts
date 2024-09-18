933b3caa460556352ce6abac579ce92c9f33c874
Taichi.N <49602502+taichi6930@users.noreply.github.com>
2024-09-18 21:31:51 +0900

    containerの整理

---
+++ lib/src/container/gatewayConfig.ts
@@ -0,0 +1,153 @@
+import { container } from 'tsyringe';
+
+import { JraPlaceData } from '../domain/jraPlaceData';
+import { JraRaceData } from '../domain/jraRaceData';
+import { NarPlaceData } from '../domain/narPlaceData';
+import { NarRaceData } from '../domain/narRaceData';
+import { JraPlaceDataHtmlGateway } from '../gateway/implement/jraPlaceDataHtmlGateway';
+import { JraRaceDataHtmlGateway } from '../gateway/implement/jraRaceDataHtmlGateway';
+import { NarPlaceDataHtmlGateway } from '../gateway/implement/narPlaceDataHtmlGateway';
+import { NarRaceDataHtmlGateway } from '../gateway/implement/narRaceDataHtmlGateway';
+import { S3Gateway } from '../gateway/implement/s3Gateway';
+import { IJraPlaceDataHtmlGateway } from '../gateway/interface/iJraPlaceDataHtmlGateway';
+import { IJraRaceDataHtmlGateway } from '../gateway/interface/iJraRaceDataHtmlGateway';
+import { INarPlaceDataHtmlGateway } from '../gateway/interface/iNarPlaceDataHtmlGateway';
+import { INarRaceDataHtmlGateway } from '../gateway/interface/iNarRaceDataHtmlGateway';
+import { IS3Gateway } from '../gateway/interface/iS3Gateway';
+import { MockS3Gateway } from '../gateway/mock/mockS3Gateway';
+
+// s3Gatewayの実装クラスをDIコンテナに登錄する
+container.register<IS3Gateway<NarRaceData>>('NarRaceS3Gateway', {
+    useFactory: () => {
+        switch (process.env.ENV) {
+            case 'production':
+                // ENV が production の場合、S3Gateway を使用
+                return new S3Gateway<NarRaceData>(
+                    'race-schedule-bucket',
+                    'nar/race/',
+                );
+            case 'local':
+                // ENV が local の場合、MockS3Gateway を使用
+                return new MockS3Gateway<NarRaceData>(
+                    'race-schedule-bucket',
+                    'nar/race/',
+                );
+            default:
+                // ENV が ない場合、MockS3Gateway を使用
+                return new MockS3Gateway<NarRaceData>(
+                    'race-schedule-bucket',
+                    'nar/race/',
+                );
+        }
+    },
+});
+container.register<IS3Gateway<JraRaceData>>('JraRaceS3Gateway', {
+    useFactory: () => {
+        switch (process.env.ENV) {
+            case 'production':
+                // ENV が production の場合、S3Gateway を使用
+                return new S3Gateway<JraRaceData>(
+                    'race-schedule-bucket',
+                    'jra/race/',
+                );
+            case 'local':
+                // ENV が local の場合、MockS3Gateway を使用
+                return new MockS3Gateway<JraRaceData>(
+                    'race-schedule-bucket',
+                    'jra/race/',
+                );
+            default:
+                // ENV が ない場合、MockS3Gateway を使用
+                return new MockS3Gateway<JraRaceData>(
+                    'race-schedule-bucket',
+                    'jra/race/',
+                );
+        }
+    },
+});
+container.register<IS3Gateway<NarPlaceData>>('NarPlaceS3Gateway', {
+    useFactory: () => {
+        console.log(`NarPlaceS3Gateway ${process.env.ENV}`);
+        switch (process.env.ENV) {
+            case 'production':
+                return new S3Gateway<NarPlaceData>(
+                    'race-schedule-bucket',
+                    'nar/place/',
+                );
+            case 'local':
+                return new MockS3Gateway<NarPlaceData>(
+                    'race-schedule-bucket',
+                    'nar/place/',
+                );
+            default:
+                return new MockS3Gateway<NarPlaceData>(
+                    'race-schedule-bucket',
+                    'nar/place/',
+                );
+        }
+    },
+});
+container.register<IS3Gateway<JraPlaceData>>('JraPlaceS3Gateway', {
+    useFactory: () => {
+        console.log(`JraPlaceS3Gateway ${process.env.ENV}`);
+        switch (process.env.ENV) {
+            case 'production':
+                return new S3Gateway<JraPlaceData>(
+                    'race-schedule-bucket',
+                    'jra/place/',
+                );
+            case 'local':
+                return new MockS3Gateway<JraPlaceData>(
+                    'race-schedule-bucket',
+                    'jra/place/',
+                );
+            default:
+                return new MockS3Gateway<JraPlaceData>(
+                    'race-schedule-bucket',
+                    'jra/place/',
+                );
+        }
+    },
+});
+container.register<INarRaceDataHtmlGateway>('NarRaceDataHtmlGateway', {
+    useFactory: () => {
+        switch (process.env.ENV) {
+            case 'production':
+                console.log('NarRaceDataHtmlGateway');
+                return new NarRaceDataHtmlGateway();
+            default:
+                return new NarRaceDataHtmlGateway();
+        }
+    },
+});
+container.register<IJraRaceDataHtmlGateway>('JraRaceDataHtmlGateway', {
+    useFactory: () => {
+        switch (process.env.ENV) {
+            case 'production':
+                console.log('JraRaceDataHtmlGateway');
+                return new JraRaceDataHtmlGateway();
+            default:
+                return new JraRaceDataHtmlGateway();
+        }
+    },
+});
+container.register<INarPlaceDataHtmlGateway>('NarPlaceDataHtmlGateway', {
+    useFactory: () => {
+        switch (process.env.ENV) {
+            case 'production':
+                return new NarPlaceDataHtmlGateway();
+            default:
+                return new NarPlaceDataHtmlGateway();
+        }
+    },
+});
+container.register<IJraPlaceDataHtmlGateway>('JraPlaceDataHtmlGateway', {
+    useFactory: () => {
+        switch (process.env.ENV) {
+            case 'production':
+                return new JraPlaceDataHtmlGateway();
+            default:
+                return new JraPlaceDataHtmlGateway();
+        }
+    },
+});
---
+++ lib/src/container/serviceConfig.ts
@@ -0,0 +1,46 @@
+import { container } from 'tsyringe';
+
+import { JraRaceData } from '../domain/jraRaceData';
+import { NarRaceData } from '../domain/narRaceData';
+import { GoogleCalendarService } from '../service/implement/googleCalendarService';
+import { ICalendarService } from '../service/interface/ICalendarService';
+import { MockGoogleCalendarService } from '../service/mock/mockGoogleCalendarService';
+
+// ICalendarServiceの実装クラスをDIコンテナに登錄する
+container.register<ICalendarService<NarRaceData>>('NarCalendarService', {
+    useFactory: () => {
+        switch (process.env.ENV) {
+            case 'production':
+                // ENV が production の場合、GoogleCalendarService を使用
+                return new GoogleCalendarService<NarRaceData>(
+                    'nar',
+                    process.env.NAR_CALENDAR_ID ?? '',
+                );
+            case 'local':
+                // ENV が local の場合、MockGoogleCalendarService を使用
+                return new MockGoogleCalendarService('nar');
+            default:
+                // ENV が指定されていない場合も MockGoogleCalendarService を使用
+                return new MockGoogleCalendarService('nar');
+        }
+    },
+});
+
+container.register<ICalendarService<JraRaceData>>('JraCalendarService', {
+    useFactory: () => {
+        switch (process.env.ENV) {
+            case 'production':
+                // ENV が production の場合、GoogleCalendarService を使用
+                return new GoogleCalendarService<JraRaceData>(
+                    'jra',
+                    process.env.JRA_CALENDAR_ID ?? '',
+                );
+            case 'local':
+                // ENV が local の場合、MockGoogleCalendarService を使用
+                return new MockGoogleCalendarService('jra');
+            default:
+                // ENV が指定されていない場合も MockGoogleCalendarService を使用
+                return new MockGoogleCalendarService('jra');
+        }
+    },
+});
