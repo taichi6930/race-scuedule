7ac504b5878a1346cb6908fbfbe6e7e0dd1b95d5
Taichi.N <49602502+taichi6930@users.noreply.github.com>
2024-09-19 23:05:36 +0900

    stackの整理

--- lib/cdk-race-schedule-app-stack.ts
+++ lib/cdk-race-schedule-app-stack.ts
@@ -1,22 +1,10 @@
-import type { StackProps } from 'aws-cdk-lib';
-import {
-    aws_apigateway,
-    aws_lambda_nodejs,
-    aws_s3,
-    CfnOutput,
-    Duration,
-    Stack,
-} from 'aws-cdk-lib';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import {
-    Effect,
-    PolicyStatement,
-    Role,
-    ServicePrincipal,
-} from 'aws-cdk-lib/aws-iam';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import type { Construct } from 'constructs';
+import { aws_s3, CfnOutput, Stack, type StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import * as dotenv from 'dotenv';
+
+import { createApiGateway } from './stack/api-setup';
+import { createLambdaExecutionRole } from './stack/iam-setup';
+import { createLambdaFunction } from './stack/lambda-setup';
 
 dotenv.config({ path: './app.env' });
 
@@ -32,99 +20,13 @@ export class CdkRaceScheduleAppStack extends Stack {
         );
 
         // Lambda実行に必要なIAMロールを作成
-        const role = new Role(this, 'LambdaExecutionRole', {
-            assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
-        });
-
-        // Lambda が S3 バケットにアクセスできるようにするポリシーステートメントを追加
-        role.addToPolicy(
-            new PolicyStatement({
-                actions: ['s3:GetObject', 's3:PutObject'],
-                resources: [bucket.bucketArn + '/*'],
-            }),
-        );
-
-        // Lambdaのログ出力権限を追加
-        role.addToPolicy(
-            new PolicyStatement({
-                actions: [
-                    'logs:CreateLogGroup',
-                    'logs:CreateLogStream',
-                    'logs:PutLogEvents',
-                ],
-                effect: Effect.ALLOW,
-                resources: ['*'],
-            }),
-        );
-
-        // Lambda が EC2 の CreateNetworkInterface アクションを実行できるようにするポリシーステートメントを追加
-        role.addToPolicy(
-            new PolicyStatement({
-                actions: ['ec2:CreateNetworkInterface'],
-                effect: Effect.ALLOW,
-                resources: ['*'], // リソースを特定のサブネットに制限する場合は、適切なリソース ARN を指定します
-            }),
-        );
-
-        // Lambda が EC2 の DescribeNetworkInterfaces アクションを実行できるようにするポリシーステートメントを追加
-        role.addToPolicy(
-            new PolicyStatement({
-                actions: ['ec2:DescribeNetworkInterfaces'],
-                effect: Effect.ALLOW,
-                resources: ['*'], // リソースを特定のネットワークインターフェースに制限する場合は、適切なリソース ARN を指定します
-            }),
-        );
-
-        // Lambda が EC2 の DeleteNetworkInterface アクションを実行できるようにするポリシーステートメントを追加
-        role.addToPolicy(
-            new PolicyStatement({
-                actions: ['ec2:DeleteNetworkInterface'],
-                effect: Effect.ALLOW,
-                resources: ['*'], // リソースを特定のネットワークインターフェースに制限する場合は、適切なリソース ARN を指定します
-            }),
-        );
+        const lambdaRole = createLambdaExecutionRole(this, bucket);
 
         // Lambda関数を作成
-        const lambdaFunction = new aws_lambda_nodejs.NodejsFunction(
-            this,
-            'RaceScheduleAppLambda',
-            {
-                architecture: lambda.Architecture.ARM_64,
-                runtime: lambda.Runtime.NODEJS_20_X,
-                entry: 'lib/src/index.ts',
-                role: role,
-                environment: {
-                    ENV: process.env.ENV ?? 'local',
-                    JRA_CALENDAR_ID: process.env.JRA_CALENDAR_ID ?? '',
-                    NAR_CALENDAR_ID: process.env.NAR_CALENDAR_ID ?? '',
-                    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL ?? '',
-                    GOOGLE_PRIVATE_KEY: (
-                        process.env.GOOGLE_PRIVATE_KEY ?? ''
-                    ).replace(/\\n/g, '\n'),
-                },
-                timeout: Duration.seconds(30),
-                memorySize: 1024,
-            },
-        );
+        const lambdaFunction = createLambdaFunction(this, lambdaRole);
 
         // API Gatewayの設定
-        const api = new apigateway.LambdaRestApi(this, 'RaceScheduleAppApi', {
-            handler: lambdaFunction,
-            defaultCorsPreflightOptions: {
-                // すべてのオリジンを許可
-                allowOrigins: aws_apigateway.Cors.ALL_ORIGINS,
-
-                // すべてのHTTPメソッドを許可
-                allowMethods: ['GET', 'POST'],
-
-                // すべてのヘッダーを許可
-                allowHeaders: ['Content-Type', 'Authorization'],
-                maxAge: Duration.minutes(1),
-            },
-            deployOptions: {
-                stageName: 'v1',
-            },
-        });
+        const api = createApiGateway(this, lambdaFunction);
 
         // API Gateway エンドポイントの出力
         new CfnOutput(this, 'RaceScheduleAppApiGatewayEndpoint', {
