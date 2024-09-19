1eec3239d6b73b6f84c5c5cf97f67d2122eab0e5
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-09-19 23:00:00 +0900

    stackを分けて管理する

---
+++ lib/stack/iam-setup.ts
@@ -0,0 +1,68 @@
+import {
+    Effect,
+    PolicyStatement,
+    Role,
+    ServicePrincipal,
+} from 'aws-cdk-lib/aws-iam';
+import * as aws_s3 from 'aws-cdk-lib/aws-s3';
+import type { Construct } from 'constructs';
+
+export function createLambdaExecutionRole(
+    scope: Construct,
+    bucket: aws_s3.IBucket,
+): Role {
+    // Lambda 実行に必要な IAM ロールを作成
+    const role = new Role(scope, 'LambdaExecutionRole', {
+        assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    // Lambda が S3 バケットにアクセスできるようにするポリシーステートメントを追加
+    role.addToPolicy(
+        new PolicyStatement({
+            actions: ['s3:GetObject', 's3:PutObject'],
+            resources: [bucket.bucketArn + '/*'],
+        }),
+    );
+
+    // Lambdaのログ出力権限を追加
+    role.addToPolicy(
+        new PolicyStatement({
+            actions: [
+                'logs:CreateLogGroup',
+                'logs:CreateLogStream',
+                'logs:PutLogEvents',
+            ],
+            effect: Effect.ALLOW,
+            resources: ['*'],
+        }),
+    );
+
+    // Lambda が EC2 の CreateNetworkInterface アクションを実行できるようにするポリシーステートメントを追加
+    role.addToPolicy(
+        new PolicyStatement({
+            actions: ['ec2:CreateNetworkInterface'],
+            effect: Effect.ALLOW,
+            resources: ['*'], // リソースを特定のサブネットに制限する場合は、適切なリソース ARN を指定します
+        }),
+    );
+
+    // Lambda が EC2 の DescribeNetworkInterfaces アクションを実行できるようにするポリシーステートメントを追加
+    role.addToPolicy(
+        new PolicyStatement({
+            actions: ['ec2:DescribeNetworkInterfaces'],
+            effect: Effect.ALLOW,
+            resources: ['*'], // リソースを特定のネットワークインターフェースに制限する場合は、適切なリソース ARN を指定します
+        }),
+    );
+
+    // Lambda が EC2 の DeleteNetworkInterface アクションを実行できるようにするポリシーステートメントを追加
+    role.addToPolicy(
+        new PolicyStatement({
+            actions: ['ec2:DeleteNetworkInterface'],
+            effect: Effect.ALLOW,
+            resources: ['*'], // リソースを特定のネットワークインターフェースに制限する場合は、適切なリソース ARN を指定します
+        }),
+    );
+
+    return role;
+}
---
+++ lib/stack/lambda-setup.ts
@@ -0,0 +1,31 @@
+import { aws_lambda_nodejs, Duration } from 'aws-cdk-lib';
+import { Role } from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import type { Construct } from 'constructs';
+
+export function createLambdaFunction(
+    scope: Construct,
+    role: Role,
+): aws_lambda_nodejs.NodejsFunction {
+    return new aws_lambda_nodejs.NodejsFunction(
+        scope,
+        'RaceScheduleAppLambda',
+        {
+            architecture: lambda.Architecture.ARM_64,
+            runtime: lambda.Runtime.NODEJS_20_X,
+            entry: 'lib/src/index.ts',
+            role: role,
+            environment: {
+                ENV: process.env.ENV ?? 'local',
+                JRA_CALENDAR_ID: process.env.JRA_CALENDAR_ID ?? '',
+                NAR_CALENDAR_ID: process.env.NAR_CALENDAR_ID ?? '',
+                GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL ?? '',
+                GOOGLE_PRIVATE_KEY: (
+                    process.env.GOOGLE_PRIVATE_KEY ?? ''
+                ).replace(/\\n/g, '\n'),
+            },
+            timeout: Duration.seconds(30),
+            memorySize: 1024,
+        },
+    );
+}
