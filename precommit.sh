94ee283eb754edc7c9eace064e3209d789a03fb5
Taichi.N <49602502+taichi6930@users.noreply.github.com>
2024-09-23 20:51:08 +0900

    環境の整備

--- precommit.sh
+++
@@ -1,9 +0,0 @@
-# pnpm run lint:fix でフォーマットエラーがある場合、エラーを出力してフォーマットを修正する
-pnpm run lint:fix
-if [ $? -ne 0 ]; then
-    echo "フォーマットエラーがあります。"
-    pnpm run lint:fix
-    echo "フォーマットを修正しました。"
-    echo "再度コミットしてください。"
-    exit 1
-fi
