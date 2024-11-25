a016d9406cdc671dc750c73d5f2b2259563ed353
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-11-25 10:41:57 +0900

    coverage対象の追加

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,13 @@ module.exports = {
     coveragePathIgnorePatterns: [
         'logger.ts',
         'lib/src/utility/env.ts',
-        // lib/src/repository/implement配下はテスト対象外
-        'lib/src/repository/implement',
+        // TODO: データ用意が大変なので一旦除外、どこかでテストしたい
+        'lib/src/repository/implement/autorace.*.ts',
+        'lib/src/repository/implement/boatrace.*.ts',
+        'lib/src/repository/implement/keirin.*.ts',
+        'lib/src/repository/implement/world.*.ts',
+        'lib/src/repository/implement/nar.*.ts',
+        'lib/src/repository/implement/jra.*.ts',
     ],
     testEnvironment: 'node',
     roots: ['<rootDir>/test'],
