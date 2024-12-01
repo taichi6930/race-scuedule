b1b5d655558349fc01723034acf4795d16478e3f
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-12-01 13:53:24 +0900

    テスト追加

---
+++ test/src/repository/implement/boatracePlaceRepositoryFromStorageImpl.test.ts
@@ -0,0 +1,120 @@
+import 'reflect-metadata';
+
+import { parse } from 'date-fns';
+import { format } from 'date-fns';
+import { container } from 'tsyringe';
+
+import { BoatracePlaceData } from '../../../../lib/src/domain/boatracePlaceData';
+import type { IS3Gateway } from '../../../../lib/src/gateway/interface/iS3Gateway';
+import type { BoatracePlaceRecord } from '../../../../lib/src/gateway/record/boatracePlaceRecord';
+import { BoatracePlaceEntity } from '../../../../lib/src/repository/entity/boatracePlaceEntity';
+import { BoatracePlaceRepositoryFromStorageImpl } from '../../../../lib/src/repository/implement/boatracePlaceRepositoryFromStorageImpl';
+import { FetchPlaceListRequest } from '../../../../lib/src/repository/request/fetchPlaceListRequest';
+import { RegisterPlaceListRequest } from '../../../../lib/src/repository/request/registerPlaceListRequest';
+import { mockS3GatewayForBoatracePlace } from '../../mock/gateway/s3GatewayMock';
+
+describe('BoatracePlaceRepositoryFromStorageImpl', () => {
+    let s3Gateway: jest.Mocked<IS3Gateway<BoatracePlaceRecord>>;
+    let repository: BoatracePlaceRepositoryFromStorageImpl;
+
+    beforeEach(() => {
+        // S3Gatewayのモックを作成
+        s3Gateway = mockS3GatewayForBoatracePlace();
+
+        // DIコンテナにモックを登録
+        container.registerInstance('BoatracePlaceS3Gateway', s3Gateway);
+
+        // テスト対象のリポジトリを生成
+        repository = container.resolve(BoatracePlaceRepositoryFromStorageImpl);
+    });
+
+    describe('fetchPlaceList', () => {
+        test('正しい競馬場データを取得できる', async () => {
+            // モックの戻り値を設定
+            s3Gateway.fetchDataFromS3.mockImplementation(
+                async (filename: string) => {
+                    // filenameから日付を取得 16時からの競馬場にしたい
+                    const date = parse(
+                        filename.slice(0, 6),
+                        'yyyyMM',
+                        new Date(),
+                    );
+                    date.setHours(16);
+                    console.log(date);
+
+                    // CSVのヘッダーを定義
+                    const csvHeaderDataText = [
+                        'dateTime',
+                        'location',
+                        'grade',
+                        'id',
+                    ].join(',');
+
+                    // データ行を生成
+                    const csvDataText: string = [
+                        format(date, 'yyyy-MM-dd HH:mm:ss'),
+                        '平塚',
+                        'GP',
+                        `boatrace${format(date, 'yyyyMM')}`,
+                    ].join(',');
+                    // データ行を生成
+                    const csvUndefinedDataText: string = [
+                        undefined,
+                        undefined,
+                        undefined,
+                        undefined,
+                    ].join(',');
+
+                    // ヘッダーとデータ行を結合して完全なCSVデータを生成
+                    const csvDatajoinText: string = [
+                        csvHeaderDataText,
+                        csvDataText,
+                        csvUndefinedDataText,
+                    ].join('\n');
+                    return Promise.resolve(csvDatajoinText);
+                },
+            );
+            // リクエストの作成
+            const request = new FetchPlaceListRequest(
+                new Date('2024-01-01'),
+                new Date('2024-02-01'),
+            );
+            // テスト実行
+            const response = await repository.fetchPlaceList(request);
+
+            // レスポンスの検証
+            expect(response.placeDataList).toHaveLength(1);
+        });
+    });
+
+    describe('registerPlaceList', () => {
+        test('正しい競馬場データを登録できる', async () => {
+            // 1年間の競馬場データを登録する
+            const placeDataList: BoatracePlaceEntity[] = Array.from(
+                { length: 366 },
+                (_, day) => {
+                    const date = new Date('2024-01-01');
+                    date.setDate(date.getDate() + day);
+                    return Array.from(
+                        { length: 12 },
+                        () =>
+                            new BoatracePlaceEntity(
+                                null,
+                                new BoatracePlaceData(date, '平和島', 'SG'),
+                            ),
+                    );
+                },
+            ).flat();
+
+            // リクエストの作成
+            const request = new RegisterPlaceListRequest<BoatracePlaceEntity>(
+                placeDataList,
+            );
+            // テスト実行
+            await repository.registerPlaceList(request);
+
+            // uploadDataToS3が12回呼ばれることを検証
+            expect(s3Gateway.uploadDataToS3).toHaveBeenCalledTimes(12);
+        });
+    });
+});
---
+++ test/src/repository/implement/boatraceRaceRepositoryFromStorageImpl.test.ts
@@ -0,0 +1,193 @@
+import 'reflect-metadata';
+
+import { format, parse } from 'date-fns';
+import { container } from 'tsyringe';
+
+import { BoatraceRaceData } from '../../../../lib/src/domain/boatraceRaceData';
+import type { IS3Gateway } from '../../../../lib/src/gateway/interface/iS3Gateway';
+import type { BoatraceRacePlayerRecord } from '../../../../lib/src/gateway/record/boatraceRacePlayerRecord';
+import type { BoatraceRaceRecord } from '../../../../lib/src/gateway/record/boatraceRaceRecord';
+import type { BoatracePlaceEntity } from '../../../../lib/src/repository/entity/boatracePlaceEntity';
+import { BoatraceRaceEntity } from '../../../../lib/src/repository/entity/boatraceRaceEntity';
+import { BoatraceRaceRepositoryFromStorageImpl } from '../../../../lib/src/repository/implement/boatraceRaceRepositoryFromStorageImpl';
+import { FetchRaceListRequest } from '../../../../lib/src/repository/request/fetchRaceListRequest';
+import { RegisterRaceListRequest } from '../../../../lib/src/repository/request/registerRaceListRequest';
+import { BOATRACE_PLACE_CODE } from '../../../../lib/src/utility/data/boatrace';
+import { baseBoatraceRacePlayerDataList } from '../../mock/common/baseData';
+import {
+    mockS3GatewayForBoatraceRace,
+    mockS3GatewayForBoatraceRacePlayer,
+} from '../../mock/gateway/s3GatewayMock';
+
+describe('BoatraceRaceRepositoryFromStorageImpl', () => {
+    let raceS3Gateway: jest.Mocked<IS3Gateway<BoatraceRaceRecord>>;
+    let racePlayerS3Gateway: jest.Mocked<IS3Gateway<BoatraceRacePlayerRecord>>;
+    let repository: BoatraceRaceRepositoryFromStorageImpl;
+
+    beforeEach(() => {
+        // S3Gatewayのモックを作成
+        raceS3Gateway = mockS3GatewayForBoatraceRace();
+        racePlayerS3Gateway = mockS3GatewayForBoatraceRacePlayer();
+
+        // DIコンテナにモックを登録
+        container.registerInstance('BoatraceRaceS3Gateway', raceS3Gateway);
+        container.registerInstance(
+            'BoatraceRacePlayerS3Gateway',
+            racePlayerS3Gateway,
+        );
+
+        // テスト対象のリポジトリを生成
+        repository = container.resolve(BoatraceRaceRepositoryFromStorageImpl);
+    });
+
+    describe('fetchRaceList', () => {
+        test('正しいレースデータを取得できる', async () => {
+            // モックの戻り値を設定
+            raceS3Gateway.fetchDataFromS3.mockImplementation(
+                async (filename: string) => {
+                    // filenameから日付を取得 16時からのレースにしたい
+                    const date = parse(
+                        filename.slice(0, 8),
+                        'yyyyMMdd',
+                        new Date(),
+                    );
+                    date.setHours(16);
+                    const csvHeaderDataText: string = [
+                        'name',
+                        'stage',
+                        'dateTime',
+                        'location',
+                        'grade',
+                        'number',
+                        'id',
+                    ].join(',');
+                    const csvDataText: string = [
+                        `raceName${filename.slice(0, 8)}`,
+                        `決勝`,
+                        date.toISOString(),
+                        '平塚',
+                        'GⅠ',
+                        '1',
+                        `boatrace${format(date, 'yyyyMMdd')}${BOATRACE_PLACE_CODE['平塚']}01`,
+                    ].join(',');
+                    const csvDataRameNameUndefinedText: string = [
+                        undefined,
+                        `決勝`,
+                        date.toISOString(),
+                        '平塚',
+                        'GⅠ',
+                        '1',
+                        `boatrace${format(date, 'yyyyMMdd')}${BOATRACE_PLACE_CODE['平塚']}01`,
+                    ].join(',');
+                    const csvDataNumUndefinedText: string = [
+                        `raceName${filename.slice(0, 8)}`,
+                        `決勝`,
+                        date.toISOString(),
+                        '平塚',
+                        'GⅠ',
+                        undefined,
+                        `boatrace${format(date, 'yyyyMMdd')}${BOATRACE_PLACE_CODE['平塚']}01`,
+                    ].join(',');
+                    const csvDatajoinText: string = [
+                        csvHeaderDataText,
+                        csvDataText,
+                        csvDataRameNameUndefinedText,
+                        csvDataNumUndefinedText,
+                    ].join('\n');
+                    return Promise.resolve(csvDatajoinText);
+                },
+            );
+            racePlayerS3Gateway.fetchDataFromS3.mockImplementation(
+                async (filename: string) => {
+                    // filenameから日付を取得 16時からのレースにしたい
+                    const date = parse(
+                        filename.slice(0, 8),
+                        'yyyyMMdd',
+                        new Date(),
+                    );
+                    date.setHours(16);
+                    const csvHeaderDataText: string = [
+                        'id',
+                        'playerId',
+                        'positionNumber',
+                        'playerNumber',
+                    ].join(',');
+                    const csvDataText: string = [
+                        `boatrace${format(date, 'yyyyMMdd')}${BOATRACE_PLACE_CODE['平塚']}0101`,
+                        `boatrace${format(date, 'yyyyMMdd')}${BOATRACE_PLACE_CODE['平塚']}01`,
+                        '1',
+                        '1',
+                    ].join(',');
+                    const csvDataRameNameUndefinedText: string = [
+                        undefined,
+                        `boatrace${format(date, 'yyyyMMdd')}${BOATRACE_PLACE_CODE['平塚']}01`,
+                        '1',
+                        '1',
+                    ].join(',');
+                    const csvDataNumUndefinedText: string = [
+                        `boatrace${format(date, 'yyyyMMdd')}${BOATRACE_PLACE_CODE['平塚']}0101`,
+                        `boatrace${format(date, 'yyyyMMdd')}${BOATRACE_PLACE_CODE['平塚']}01`,
+                        null,
+                        '1',
+                    ].join(',');
+                    const csvDatajoinText: string = [
+                        csvHeaderDataText,
+                        csvDataText,
+                        csvDataRameNameUndefinedText,
+                        csvDataNumUndefinedText,
+                    ].join('\n');
+                    return Promise.resolve(csvDatajoinText);
+                },
+            );
+            // リクエストの作成
+            const request = new FetchRaceListRequest<BoatracePlaceEntity>(
+                new Date('2024-01-01'),
+                new Date('2024-02-01'),
+            );
+            // テスト実行
+            const response = await repository.fetchRaceList(request);
+
+            // レスポンスの検証
+            expect(response.raceDataList).toHaveLength(32);
+        });
+    });
+
+    describe('registerRaceList', () => {
+        test('正しいレースデータを登録できる', async () => {
+            // 1年間のレースデータを登録する
+            const raceDataList: BoatraceRaceEntity[] = Array.from(
+                { length: 366 },
+                (_, day) => {
+                    const date = new Date('2024-01-01');
+                    date.setDate(date.getDate() + day);
+                    return Array.from(
+                        { length: 12 },
+                        (__, j) =>
+                            new BoatraceRaceEntity(
+                                null,
+                                new BoatraceRaceData(
+                                    `raceName${format(date, 'yyyyMMdd')}`,
+                                    `優勝戦`,
+                                    date,
+                                    '平和島',
+                                    'GⅠ',
+                                    j + 1,
+                                ),
+                                baseBoatraceRacePlayerDataList,
+                            ),
+                    );
+                },
+            ).flat();
+
+            // リクエストの作成
+            const request = new RegisterRaceListRequest<BoatraceRaceEntity>(
+                raceDataList,
+            );
+            // テスト実行
+            await repository.registerRaceList(request);
+
+            // uploadDataToS3が366回呼ばれることを検証
+            expect(raceS3Gateway.uploadDataToS3).toHaveBeenCalledTimes(366);
+        });
+    });
+});
