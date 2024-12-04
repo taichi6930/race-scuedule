2334f697952673875ae3c1d1395f77c4c8a792f0
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-12-04 22:00:20 +0900

    ソースの不具合修正

--- lib/src/repository/mock/mockKeirinRaceRepositoryFromHtmlImpl.ts
+++ lib/src/repository/mock/mockKeirinRaceRepositoryFromHtmlImpl.ts
@@ -1,4 +1,4 @@
-import { baseKeirinRacePlayerDataList } from '../../../../test/src/mock/common/baseBoatraceData';
+import { baseKeirinRacePlayerDataList } from '../../../../test/src/mock/common/baseKeirinData';
 import { KeirinPlaceData } from '../../domain/keirinPlaceData';
 import { KeirinRaceData } from '../../domain/keirinRaceData';
 import { Logger } from '../../utility/logger';
