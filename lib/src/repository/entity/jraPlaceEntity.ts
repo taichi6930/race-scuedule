9fb13d86987e66774c88c97aba4f9beb6686996d
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-12-08 18:29:58 +0900

    jraのplaceファイルを1つにする

--- lib/src/repository/entity/jraPlaceEntity.ts
+++ lib/src/repository/entity/jraPlaceEntity.ts
@@ -1,4 +1,5 @@
 import type { JraPlaceData } from '../../domain/jraPlaceData';
+import { JraPlaceRecord } from '../../gateway/record/jraPlaceRecord';
 import type { JraPlaceId } from '../../utility/raceId';
 import { generateJraPlaceId } from '../../utility/raceId';
 
@@ -35,6 +36,20 @@ export class JraPlaceEntity {
         return new JraPlaceEntity(
             partial.id ?? null,
             partial.placeData ?? this.placeData,
+        );
+    }
+
+    /**
+     * JraPlaceRecordに変換する
+     * @returns
+     */
+    toRecord(): JraPlaceRecord {
+        return new JraPlaceRecord(
+            this.id,
+            this.placeData.dateTime,
+            this.placeData.location,
+            this.placeData.heldTimes,
+            this.placeData.heldDayTimes,
         );
     }
 }
