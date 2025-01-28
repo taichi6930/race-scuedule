9e1b892d788bea13c758cf05096737bd99185838
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-01-28 12:36:17 +0900

    GoogleCalendarGatewayの作成

---
+++ lib/src/gateway/implement/googleCalendarGateway.ts
@@ -0,0 +1,54 @@
+/**
+ * JRAの競馬場データのHTMLを取得するGatewayの実装
+ */
+
+import type { JWT } from 'google-auth-library';
+import type { calendar_v3 } from 'googleapis';
+import { google } from 'googleapis';
+
+import type { ICalendarGateway } from '../interface/iCalendarGateway';
+
+export class GoogleCalendarGateway implements ICalendarGateway {
+    private readonly credentials: JWT;
+    private readonly calendar: calendar_v3.Calendar;
+    private readonly calendarId: string;
+
+    constructor(calendarId: string) {
+        this.credentials = new google.auth.JWT(
+            // client_emailは環境変数から取得
+            process.env.GOOGLE_CLIENT_EMAIL,
+            undefined,
+            // private_keyは環境変数から取得
+            process.env.GOOGLE_PRIVATE_KEY,
+            ['https://www.googleapis.com/auth/calendar'],
+        );
+        this.calendar = google.calendar({
+            version: 'v3',
+            auth: this.credentials,
+        });
+        this.calendarId = calendarId;
+    }
+
+    async fetchCalendarDataList(
+        startDate: Date,
+        finishDate: Date,
+    ): Promise<calendar_v3.Schema$Event[]> {
+        try {
+            // orderBy: 'startTime'で開始時刻順に取得
+            const response = await this.calendar.events.list({
+                calendarId: this.calendarId,
+                timeMin: startDate.toISOString(),
+                timeMax: finishDate.toISOString(),
+                singleEvents: true,
+                orderBy: 'startTime',
+            });
+            return response.data.items ?? [];
+        } catch (error) {
+            console.error(
+                'Google Calendar APIからのイベント取得に失敗しました',
+                error,
+            );
+            return [];
+        }
+    }
+}
