ed439d4249ed095175609cea0fbf2ecd7e162120
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-01-30 22:31:16 +0900

    WorldGoogleCalendarGatewayのcontainer追加

--- lib/container/gateway/calendarGatewayConfig.ts
+++ lib/container/gateway/calendarGatewayConfig.ts
@@ -1,5 +1,6 @@
 import { container } from 'tsyringe';
 
+import { GoogleCalendarGateway } from '../../src/gateway/implement/googleCalendarGateway';
 import type { ICalendarGateway } from '../../src/gateway/interface/iCalendarGateway';
 import { MockGoogleCalendarGateway } from '../../src/gateway/mock/mockGoogleCalendarGateway';
 import { ENV } from '../../src/utility/env';
@@ -8,7 +9,13 @@ container.register<ICalendarGateway>('WorldGoogleCalendarGateway', {
     useFactory: () => {
         switch (ENV) {
             case 'PRODUCTION':
-                return new MockGoogleCalendarGateway('world');
+                return new GoogleCalendarGateway(
+                    process.env.WORLD_CALENDAR_ID ?? '',
+                );
+            case 'TEST':
+                return new GoogleCalendarGateway(
+                    process.env.TEST_CALENDAR_ID ?? '',
+                );
             case 'LOCAL':
                 return new MockGoogleCalendarGateway('world');
             default:
