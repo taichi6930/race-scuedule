45e54ba00496f412db39df5d998e7113d6497d81
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-02-01 13:33:15 +0900

    jra, narのEntityテスト追加

--- test/src/mock/common/baseNarData.ts
+++ test/src/mock/common/baseNarData.ts
@@ -1,3 +1,5 @@
+import type { calendar_v3 } from 'googleapis';
+
 import { CalendarData } from '../../../../lib/src/domain/calendarData';
 import { NarPlaceData } from '../../../../lib/src/domain/narPlaceData';
 import { NarRaceData } from '../../../../lib/src/domain/narRaceData';
@@ -72,6 +74,52 @@ export const baseNarRaceEntity = new NarRaceEntity(
     baseNarRaceData,
     baseNarRaceUpdateDate,
 );
+
+export const baseNarGoogleCalendarData: calendar_v3.Schema$Event = {
+    id: generateNarRaceId(
+        baseNarPlaceDateTime,
+        baseNarPlaceCourse,
+        baseNarRaceNumber,
+    ),
+    summary: baseNarRaceName,
+    start: {
+        dateTime: baseNarRaceDateTime.toISOString().replace('Z', '+09:00'),
+        timeZone: 'Asia/Tokyo',
+    },
+    end: {
+        dateTime: new Date(baseNarRaceDateTime.getTime() + 10 * 60 * 1000)
+            .toISOString()
+            .replace('Z', '+09:00'),
+        timeZone: 'Asia/Tokyo',
+    },
+    location: `${baseNarPlaceCourse}競馬場`,
+    colorId: '9',
+    description: `距離: ダート2000m
+発走: 15:40
+<a href="http://keiba-lv-st.jp/">レース映像（地方競馬LIVE）</a>
+<a href="https://www.youtube.com/@tckkeiba/stream">レース映像（YouTube）</a>
+<a href="https://netkeiba.page.link/?link=https%3A%2F%2Fnar.sp.netkeiba.com%2Frace%2Fshutuba.html%3Frace_id%3D202444122911">レース情報（netkeiba）</a>
+<a href="https://www2.keiba.go.jp/KeibaWeb/TodayRaceInfo/DebaTable?k_raceDate=2024%2f12%2f29&k_raceNo=11&k_babaCode=20">レース情報（NAR）</a>
+更新日時: 2025/01/01 21:00:00
+`,
+    extendedProperties: {
+        private: {
+            dateTime: baseNarRaceDateTime.toISOString(),
+            distance: baseNarRaceDistance.toString(),
+            grade: baseNarRaceGrade,
+            location: baseNarPlaceCourse,
+            name: baseNarRaceName,
+            number: baseNarRaceNumber.toString(),
+            raceId: generateNarRaceId(
+                baseNarPlaceDateTime,
+                baseNarPlaceCourse,
+                baseNarRaceNumber,
+            ),
+            surfaceType: baseNarRaceSurfaceType,
+            updateDate: baseNarRaceUpdateDate.toISOString(),
+        },
+    },
+};
 
 export const baseNarRaceEntityList: NarRaceEntity[] = ['大井', '高知'].flatMap(
     (location) => {
--- test/src/repository/entity/narRaceEntity.test.ts
+++ test/src/repository/entity/narRaceEntity.test.ts
@@ -1,6 +1,9 @@
+import { NarRaceEntity } from '../../../../lib/src/repository/entity/narRaceEntity';
 import {
+    baseNarGoogleCalendarData,
     baseNarRaceData,
     baseNarRaceEntity,
+    baseNarRaceRecord,
 } from '../../mock/common/baseNarData';
 
 describe('NarRaceEntityクラスのテスト', () => {
@@ -29,5 +32,56 @@ describe('NarRaceEntityクラスのテスト', () => {
         const raceData = raceEntity.raceData;
         // インスタンスが変更されていないか確認
         expect(raceData).toEqual(baseNarRaceData);
+    });
+
+    it('NarRaceEntityのインスタンスをGoogleカレンダーのイベントに変換できることを確認', () => {
+        const raceEntity = baseRaceEntity;
+        const googleCalendarData = raceEntity.toGoogleCalendarData(
+            new Date('2025-01-01T12:00:00.000Z'),
+        );
+        // Googleカレンダーのイベントが正しいか確認
+        expect(googleCalendarData).toEqual(baseNarGoogleCalendarData);
+    });
+
+    it('NarRaceEntityのインスタンスをNarRaceRecordに変換できることを確認', () => {
+        const raceEntity = baseNarRaceEntity;
+        const raceRecord = raceEntity.toRecord();
+        // NarRaceRecordが正しいか確認
+        expect(raceRecord).toEqual(baseNarRaceRecord);
+    });
+
+    it('GoogleカレンダーのイベントからNarRaceEntityのインスタンスを作成できることを確認', () => {
+        const raceEntity = baseNarRaceEntity;
+        const googleCalendarData = raceEntity.toGoogleCalendarData(
+            new Date('2025-01-01T12:00:00.000Z'),
+        );
+        const newRaceEntity =
+            NarRaceEntity.fromGoogleCalendarDataToRaceEntity(
+                googleCalendarData,
+            );
+        // インスタンスが正しいか確認
+        expect(newRaceEntity).toEqual(raceEntity);
+    });
+
+    it('GoogleカレンダーのイベントからCalendarDataのインスタンスを作成できることを確認', () => {
+        const raceEntity = baseNarRaceEntity;
+        const googleCalendarData = raceEntity.toGoogleCalendarData(
+            new Date('2025-01-01T12:00:00.000Z'),
+        );
+        const calendarData =
+            NarRaceEntity.fromGoogleCalendarDataToCalendarData(
+                googleCalendarData,
+            );
+        // CalendarDataが正しいか確認
+        expect(calendarData.id).toBe(googleCalendarData.id);
+        expect(calendarData.title).toBe(googleCalendarData.summary);
+        expect(calendarData.startTime).toEqual(
+            new Date(googleCalendarData.start?.dateTime ?? ''),
+        );
+        expect(calendarData.endTime).toEqual(
+            new Date(googleCalendarData.end?.dateTime ?? ''),
+        );
+        expect(calendarData.location).toBe(googleCalendarData.location);
+        expect(calendarData.description).toBe(googleCalendarData.description);
     });
 });
