531687e810bbf87811a1f8a9a68300b664d23150
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-02-05 22:13:13 +0900

    RaceDataServiceのBase作成

--- lib/src/service/implement/jraRaceDataService.ts
+++ lib/src/service/implement/jraRaceDataService.ts
@@ -3,81 +3,25 @@ import { inject, injectable } from 'tsyringe';
 import { JraPlaceEntity } from '../../repository/entity/jraPlaceEntity';
 import { JraRaceEntity } from '../../repository/entity/jraRaceEntity';
 import { IRaceRepository } from '../../repository/interface/IRaceRepository';
-import { FetchRaceListRequest } from '../../repository/request/fetchRaceListRequest';
-import { RegisterRaceListRequest } from '../../repository/request/registerRaceListRequest';
-import { FetchRaceListResponse } from '../../repository/response/fetchRaceListResponse';
-import { DataLocation, DataLocationType } from '../../utility/dataType';
-import { Logger } from '../../utility/logger';
-import { IRaceDataService } from '../interface/IRaceDataService';
+import { BaseRaceDataService } from './baseRaceDataService';
 
 @injectable()
-export class JraRaceDataService
-    implements IRaceDataService<JraRaceEntity, JraPlaceEntity>
-{
+export class JraRaceDataService extends BaseRaceDataService<
+    JraRaceEntity,
+    JraPlaceEntity
+> {
     constructor(
         @inject('JraRaceRepositoryFromStorage')
-        private readonly jraRaceRepositoryFromStorage: IRaceRepository<
+        protected readonly raceRepositoryFromStorage: IRaceRepository<
             JraRaceEntity,
             JraPlaceEntity
         >,
         @inject('JraRaceRepositoryFromHtml')
-        private readonly jraRaceRepositoryFromHtml: IRaceRepository<
+        protected readonly raceRepositoryFromHtml: IRaceRepository<
             JraRaceEntity,
             JraPlaceEntity
         >,
-    ) {}
-
-    /**
-     * レース開催データを取得する
-     *
-     * @param startDate
-     * @param finishDate
-     * @returns
-     */
-    @Logger
-    async fetchRaceEntityList(
-        startDate: Date,
-        finishDate: Date,
-        type: DataLocationType,
-        placeEntityList?: JraPlaceEntity[],
-    ): Promise<JraRaceEntity[]> {
-        try {
-            const fetchRaceListRequest =
-                new FetchRaceListRequest<JraPlaceEntity>(
-                    startDate,
-                    finishDate,
-                    placeEntityList,
-                );
-            const repository =
-                type === DataLocation.Storage
-                    ? this.jraRaceRepositoryFromStorage
-                    : this.jraRaceRepositoryFromHtml;
-
-            const fetchRaceListResponse: FetchRaceListResponse<JraRaceEntity> =
-                await repository.fetchRaceEntityList(fetchRaceListRequest);
-            return fetchRaceListResponse.raceEntityList;
-        } catch (error) {
-            console.error('レースデータの取得に失敗しました', error);
-            return [];
-        }
-    }
-
-    /**
-     * レース開催データを更新する
-     * @param raceEntityList
-     */
-    @Logger
-    async updateRaceEntityList(raceEntityList: JraRaceEntity[]): Promise<void> {
-        try {
-            // EntityListが空の場合は更新処理を行わない
-            if (raceEntityList.length === 0) return;
-            const registerRaceListRequest =
-                new RegisterRaceListRequest<JraRaceEntity>(raceEntityList);
-            await this.jraRaceRepositoryFromStorage.registerRaceEntityList(
-                registerRaceListRequest,
-            );
-        } catch (error) {
-            console.error('レースデータの更新に失敗しました', error);
-        }
+    ) {
+        super();
     }
 }
--- lib/src/service/implement/keirinRaceDataService.ts
+++ lib/src/service/implement/keirinRaceDataService.ts
@@ -3,83 +3,25 @@ import { inject, injectable } from 'tsyringe';
 import { KeirinPlaceEntity } from '../../repository/entity/keirinPlaceEntity';
 import { KeirinRaceEntity } from '../../repository/entity/keirinRaceEntity';
 import { IRaceRepository } from '../../repository/interface/IRaceRepository';
-import { FetchRaceListRequest } from '../../repository/request/fetchRaceListRequest';
-import { RegisterRaceListRequest } from '../../repository/request/registerRaceListRequest';
-import { FetchRaceListResponse } from '../../repository/response/fetchRaceListResponse';
-import { DataLocation, DataLocationType } from '../../utility/dataType';
-import { Logger } from '../../utility/logger';
-import { IRaceDataService } from '../interface/IRaceDataService';
+import { BaseRaceDataService } from './baseRaceDataService';
 
 @injectable()
-export class KeirinRaceDataService
-    implements IRaceDataService<KeirinRaceEntity, KeirinPlaceEntity>
-{
+export class KeirinRaceDataService extends BaseRaceDataService<
+    KeirinRaceEntity,
+    KeirinPlaceEntity
+> {
     constructor(
         @inject('KeirinRaceRepositoryFromStorage')
-        private readonly keirinRaceRepositoryFromStorage: IRaceRepository<
+        protected readonly raceRepositoryFromStorage: IRaceRepository<
             KeirinRaceEntity,
             KeirinPlaceEntity
         >,
         @inject('KeirinRaceRepositoryFromHtml')
-        private readonly keirinRaceRepositoryFromHtml: IRaceRepository<
+        protected readonly raceRepositoryFromHtml: IRaceRepository<
             KeirinRaceEntity,
             KeirinPlaceEntity
         >,
-    ) {}
-
-    /**
-     * レース開催データを取得する
-     *
-     * @param startDate
-     * @param finishDate
-     * @returns
-     */
-    @Logger
-    async fetchRaceEntityList(
-        startDate: Date,
-        finishDate: Date,
-        type: DataLocationType,
-        placeEntityList?: KeirinPlaceEntity[],
-    ): Promise<KeirinRaceEntity[]> {
-        try {
-            const fetchRaceListRequest =
-                new FetchRaceListRequest<KeirinPlaceEntity>(
-                    startDate,
-                    finishDate,
-                    placeEntityList,
-                );
-            const repository =
-                type === DataLocation.Storage
-                    ? this.keirinRaceRepositoryFromStorage
-                    : this.keirinRaceRepositoryFromHtml;
-
-            const fetchRaceListResponse: FetchRaceListResponse<KeirinRaceEntity> =
-                await repository.fetchRaceEntityList(fetchRaceListRequest);
-            return fetchRaceListResponse.raceEntityList;
-        } catch (error) {
-            console.error('レースデータの取得に失敗しました', error);
-            return [];
-        }
-    }
-
-    /**
-     * レース開催データを更新する
-     * @param raceEntityList
-     */
-    @Logger
-    async updateRaceEntityList(
-        raceEntityList: KeirinRaceEntity[],
-    ): Promise<void> {
-        try {
-            // EntityListが空の場合は更新処理を行わない
-            if (raceEntityList.length === 0) return;
-            const registerRaceListRequest =
-                new RegisterRaceListRequest<KeirinRaceEntity>(raceEntityList);
-            await this.keirinRaceRepositoryFromStorage.registerRaceEntityList(
-                registerRaceListRequest,
-            );
-        } catch (error) {
-            console.error('レースデータの更新に失敗しました', error);
-        }
+    ) {
+        super();
     }
 }
