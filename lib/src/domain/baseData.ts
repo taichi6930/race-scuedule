0b9729f11edebbf3b1a0b2a72fad6df285003382
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-02-06 20:22:15 +0900

    不要なPlaceDataの定義を削除

--- lib/src/domain/baseData.ts
+++ lib/src/domain/baseData.ts
@@ -1,14 +1,8 @@
-import type { AutoracePlaceData } from './autoracePlaceData';
 import type { AutoraceRaceData } from './autoraceRaceData';
-import type { BoatracePlaceData } from './boatracePlaceData';
 import type { BoatraceRaceData } from './boatraceRaceData';
-import type { JraPlaceData } from './jraPlaceData';
 import type { JraRaceData } from './jraRaceData';
-import type { KeirinPlaceData } from './keirinPlaceData';
 import type { KeirinRaceData } from './keirinRaceData';
-import type { NarPlaceData } from './narPlaceData';
 import type { NarRaceData } from './narRaceData';
-import type { WorldPlaceData } from './worldPlaceData';
 import type { WorldRaceData } from './worldRaceData';
 
 export type RaceData =
@@ -18,11 +12,3 @@ export type RaceData =
     | WorldRaceData
     | AutoraceRaceData
     | BoatraceRaceData;
-
-export type PlaceData =
-    | JraPlaceData
-    | NarPlaceData
-    | KeirinPlaceData
-    | WorldPlaceData
-    | AutoracePlaceData
-    | BoatracePlaceData;
