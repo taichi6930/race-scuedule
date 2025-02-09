2b3067846a800d9c8ce918a504f45e5ca760e50e
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-02-09 17:09:22 +0900

    不要なswaggerを削除

--- lib/src/controller/keirinRaceController.ts
+++ lib/src/controller/keirinRaceController.ts
@@ -60,82 +60,6 @@ export class KeirinRaceController {
      * 競輪カレンダーからレース情報を取得する
      * @param req リクエスト
      * @param res レスポンス
-     * @returns
-     * @swagger
-     * /api/races/keirin/calendar:
-     *   get:
-     *     description: カレンダーからレース情報を取得する
-     *     parameters:
-     *       - name: startDate
-     *         in: query
-     *         description: レース情報の開始日
-     *         required: true
-     *         schema:
-     *           type: string
-     *           format: date
-     *       - name: finishDate
-     *         in: query
-     *         description: レース情報の終了日
-     *         required: true
-     *         schema:
-     *           type: string
-     *           format: date
-     *     responses:
-     *       200:
-     *         description: レース情報を取得
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: array
-     *               items:
-     *                 type: object
-     *                 properties:
-     *                   id:
-     *                     type: string
-     *                     description: レースID
-     *                   title:
-     *                     type: string
-     *                     description: レースタイトル
-     *                   startTime:
-     *                     type: string
-     *                     format: date-time
-     *                     description: レース開始時刻
-     *                   endTime:
-     *                     type: string
-     *                     format: date-time
-     *                     description: レース終了時刻
-     *                   location:
-     *                     type: string
-     *                     description: 競輪場の名称
-     *                   description:
-     *                     type: string
-     *                     description: レースの説明
-     *       400:
-     *         description: 不正なリクエスト。`startDate` または `finishDate` が指定されていない場合
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   description: エラーメッセージ `startDate`、`finishDate` は必須です
-     *                 details:
-     *                   type: string
-     *                   description: エラーの詳細（任意でより具体的な説明を提供することができます）
-     *       500:
-     *         description: サーバーエラー。カレンダーからのレース情報取得中にエラーが発生した場合
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   description: エラーメッセージ サーバーエラーが発生しました
-     *                 details:
-     *                   type: string
-     *                   description: エラーの詳細（任意でより具体的な説明を提供することができます）
      */
     @Logger
     private async getRacesFromCalendar(
@@ -178,33 +102,6 @@ export class KeirinRaceController {
      * カレンダーにレース情報を更新する
      * @param req
      * @param res
-     * @returns
-     * @swagger
-     * /api/races/keirin/calendar:
-     *   post:
-     *     description: カレンダーにレース情報を更新する
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             properties:
-     *               startDate:
-     *                 type: string
-     *                 format: date-time
-     *                 description: レース情報の開始日
-     *               finishDate:
-     *                 type: string
-     *                 format: date-time
-     *                 description: レース情報の終了日
-     *     responses:
-     *       200:
-     *         description: レース情報を更新
-     *       400:
-     *         description: 不正なリクエスト。`startDate` または `finishDate` が指定されていない場合
-     *       500:
-     *         description: サーバーエラー。カレンダーへのレース情報更新中にエラーが発生した場合
      */
     @Logger
     private async updateRacesToCalendar(
@@ -247,84 +144,6 @@ export class KeirinRaceController {
      * レース情報を取得する
      * @param req
      * @param res
-     * @returns
-     * @swagger
-     * /api/races/keirin/race:
-     *   get:
-     *     description: レース情報を取得する
-     *     parameters:
-     *       - name: startDate
-     *         in: query
-     *         description: レース情報の開始日
-     *         required: true
-     *         schema:
-     *           type: string
-     *           format: date
-     *       - name: finishDate
-     *         in: query
-     *         description: レース情報の終了日
-     *         required: true
-     *         schema:
-     *           type: string
-     *           format: date
-     *     responses:
-     *       200:
-     *         description: レース情報を取得
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: array
-     *               items:
-     *                 type: object
-     *                 properties:
-     *                   name:
-     *                     type: string
-     *                     description: レース名
-     *                   stage:
-     *                     type: string
-     *                     description: ステージ（決勝、準決勝、予選など）
-     *                   dateTime:
-     *                     type: string
-     *                     format: date-time
-     *                     description: レース開始時刻
-     *                   location:
-     *                     type: string
-     *                     description: 競輪場の名称
-     *                   distance:
-     *                     type: number
-     *                     description: 距離
-     *                   grade:
-     *                     type: string
-     *                     description: レースのグレード
-     *                   number:
-     *                     type: number
-     *                     description: レース番号
-     *       400:
-     *         description: 不正なリクエスト。`startDate` または `finishDate` が指定されていない場合
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   description: エラーメッセージ `startDate`、`finishDate` は必須です
-     *                 details:
-     *                   type: string
-     *                   description: エラーの詳細（任意でより具体的な説明を提供することができます）
-     *       500:
-     *         description: サーバーエラー。カレンダーからのレース情報取得中にエラーが発生した場合
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   description: エラーメッセージ サーバーエラーが発生しました
-     *                 details:
-     *                   type: string
-     *                   description: エラーの詳細（任意でより具体的な説明を提供することができます）
      */
     @Logger
     private async getRaceDataList(req: Request, res: Response): Promise<void> {
@@ -395,32 +214,6 @@ export class KeirinRaceController {
 
     /**
      * レース情報を更新する
-     * @swagger
-     * /api/races/keirin/race:
-     *   post:
-     *     description: レース情報を更新する
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             properties:
-     *               startDate:
-     *                 type: string
-     *                 format: date-time
-     *                 description: レース情報の開始日
-     *               finishDate:
-     *                 type: string
-     *                 format: date-time
-     *                 description: レース情報の終了日
-     *     responses:
-     *       200:
-     *         description: レース情報を更新
-     *       400:
-     *         description: 不正なリクエスト。`startDate` または `finishDate` が指定されていない場合
-     *       500:
-     *         description: サーバーエラー。カレンダーへのレース情報更新中にエラーが発生した場合
      */
     @Logger
     private async updateRaceDataList(
@@ -465,69 +258,6 @@ export class KeirinRaceController {
      * 競輪場情報を取得する
      * @param req
      * @param res
-     * @returns
-     * @swagger
-     * /api/races/keirin/place:
-     *   get:
-     *     description: 競輪場情報を取得する
-     *     parameters:
-     *       - name: startDate
-     *         in: query
-     *         description: レース情報の開始日
-     *         required: true
-     *         schema:
-     *           type: string
-     *           format: date
-     *       - name: finishDate
-     *         in: query
-     *         description: レース情報の終了日
-     *         required: true
-     *         schema:
-     *           type: string
-     *           format: date
-     *     responses:
-     *       200:
-     *         description: レース情報を取得
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: array
-     *               items:
-     *                 type: object
-     *                 properties:
-     *                   dateTime:
-     *                     type: string
-     *                     format: date-time
-     *                     description: レース開始時刻
-     *                   location:
-     *                     type: string
-     *                     description: 競輪場の名称
-     *       400:
-     *         description: 不正なリクエスト。`startDate` または `finishDate` が指定されていない場合
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   description: エラーメッセージ `startDate`、`finishDate` は必須です
-     *                 details:
-     *                   type: string
-     *                   description: エラーの詳細（任意でより具体的な説明を提供することができます）
-     *       500:
-     *         description: サーバーエラー。カレンダーからのレース情報取得中にエラーが発生した場合
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   description: エラーメッセージ サーバーエラーが発生しました
-     *                 details:
-     *                   type: string
-     *                   description: エラーの詳細（任意でより具体的な説明を提供することができます）
      */
     @Logger
     private async getPlaceDataList(req: Request, res: Response): Promise<void> {
@@ -564,33 +294,6 @@ export class KeirinRaceController {
      * 競輪場情報を更新する
      * @param req
      * @param res
-     * @returns
-     * @swagger
-     * /api/races/keirin/place:
-     *   post:
-     *     description: 競輪場情報を更新する
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             properties:
-     *               startDate:
-     *                 type: string
-     *                 format: date-time
-     *                 description: レース情報の開始日
-     *               finishDate:
-     *                 type: string
-     *                 format: date-time
-     *                 description: レース情報の終了日
-     *     responses:
-     *       200:
-     *         description: 競輪場情報を更新
-     *       400:
-     *         description: 不正なリクエスト。`startDate` または `finishDate` が指定されていない場合
-     *       500:
-     *         description: サーバーエラー。カレンダーへのレース情報更新中にエラーが発生した場合
      */
     @Logger
     private async updatePlaceDataList(
