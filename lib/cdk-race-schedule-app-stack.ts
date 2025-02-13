7feef2afa0e0ab0bd77b803ca97d9a51e3a57215
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-02-13 13:51:48 +0900

    stackの整理

--- lib/cdk-race-schedule-app-stack.ts
+++ lib/cdk-race-schedule-app-stack.ts
@@ -1,4 +1,6 @@
 import {
+    aws_ec2,
+    aws_efs,
     aws_lambda_nodejs,
     aws_s3,
     CfnOutput,
@@ -26,6 +28,62 @@ export class CdkRaceScheduleAppStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        // VPCの作成
+        const vpc = new aws_ec2.Vpc(this, 'RaceScheduleVpc', {
+            maxAzs: 2,
+        });
+
+        // Lambdaのセキュリティグループの作成
+        const lambdaSecurityGroup = new aws_ec2.SecurityGroup(
+            this,
+            'LambdaSG',
+            {
+                vpc: vpc,
+                description: 'Lambda function security group',
+                allowAllOutbound: true,
+            },
+        );
+
+        // EFSのセキュリティグループの作成
+        const efsSecurityGroup = new aws_ec2.SecurityGroup(this, 'EfsSG', {
+            vpc: vpc,
+            description: 'EFS security group',
+        });
+
+        // EFSの作成
+        const fileSystem = new aws_efs.FileSystem(
+            this,
+            'RaceScheduleFileSystem',
+            {
+                vpc: vpc,
+                lifecyclePolicy: aws_efs.LifecyclePolicy.AFTER_7_DAYS,
+                securityGroup: efsSecurityGroup,
+            },
+        );
+
+        // EFS アクセスポイントの作成
+        const accessPoint = fileSystem.addAccessPoint('AccessPoint', {
+            path: '/lambda',
+            posixUser: {
+                uid: '1001',
+                gid: '1001',
+            },
+            createAcl: {
+                ownerUid: '1001',
+                ownerGid: '1001',
+                permissions: '755',
+            },
+        });
+
+        // 各サブネットにEFSのマウントターゲットを作成
+        vpc.publicSubnets.forEach((subnet, index) => {
+            new aws_efs.CfnMountTarget(this, `MountTarget${index.toString()}`, {
+                fileSystemId: fileSystem.fileSystemId,
+                subnetId: subnet.subnetId,
+                securityGroups: [efsSecurityGroup.securityGroupId],
+            });
+        });
+
         // S3バケットの参照
         const bucket = aws_s3.Bucket.fromBucketName(
             this,
@@ -34,10 +92,44 @@ export class CdkRaceScheduleAppStack extends Stack {
         );
 
         // Lambda実行に必要なIAMロールを作成
-        const lambdaRole = createLambdaExecutionRole(this, bucket);
+        const lambdaRole = createLambdaExecutionRole(this, bucket, fileSystem);
 
         // Lambda関数を作成
-        const lambdaFunction = createLambdaFunction(this, lambdaRole);
+        const lambdaFunction = new aws_lambda_nodejs.NodejsFunction(
+            this,
+            'RaceScheduleAppLambda',
+            {
+                architecture: lambda.Architecture.ARM_64,
+                runtime: lambda.Runtime.NODEJS_20_X,
+                entry: 'lib/src/index.ts',
+                role: lambdaRole,
+                vpc: vpc,
+                environment: {
+                    ENV: ENV ?? allowedEnvs.local,
+                    JRA_CALENDAR_ID: process.env.JRA_CALENDAR_ID ?? '',
+                    NAR_CALENDAR_ID: process.env.NAR_CALENDAR_ID ?? '',
+                    KEIRIN_CALENDAR_ID: process.env.KEIRIN_CALENDAR_ID ?? '',
+                    WORLD_CALENDAR_ID: process.env.WORLD_CALENDAR_ID ?? '',
+                    AUTORACE_CALENDAR_ID:
+                        process.env.AUTORACE_CALENDAR_ID ?? '',
+                    BOATRACE_CALENDAR_ID:
+                        process.env.BOATRACE_CALENDAR_ID ?? '',
+                    TEST_CALENDAR_ID: process.env.TEST_CALENDAR_ID ?? '',
+                    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL ?? '',
+                    GOOGLE_PRIVATE_KEY: (
+                        process.env.GOOGLE_PRIVATE_KEY ?? ''
+                    ).replace(/\\n/g, '\n'),
+                    MOUNT_PATH: '/mnt/efs',
+                },
+                timeout: Duration.seconds(90),
+                memorySize: 1024,
+                securityGroups: [lambdaSecurityGroup],
+                filesystem: lambda.FileSystem.fromEfsAccessPoint(
+                    accessPoint,
+                    '/mnt/efs',
+                ),
+            },
+        );
 
         // API Gatewayの設定
         const api = createApiGateway(this, lambdaFunction);
@@ -75,6 +167,7 @@ function createApiGateway(
 function createLambdaExecutionRole(
     scope: Construct,
     bucket: aws_s3.IBucket,
+    fileSystem: aws_efs.IFileSystem,
 ): Role {
     // Lambda 実行に必要な IAM ロールを作成
     const role = new Role(scope, 'LambdaExecutionRole', {
@@ -129,37 +222,16 @@ function createLambdaExecutionRole(
         }),
     );
 
-    return role;
-}
-
-function createLambdaFunction(
-    scope: Construct,
-    role: Role,
-): aws_lambda_nodejs.NodejsFunction {
-    return new aws_lambda_nodejs.NodejsFunction(
-        scope,
-        'RaceScheduleAppLambda',
-        {
-            architecture: lambda.Architecture.ARM_64,
-            runtime: lambda.Runtime.NODEJS_20_X,
-            entry: 'lib/src/index.ts',
-            role: role,
-            environment: {
-                ENV: ENV ?? allowedEnvs.local,
-                JRA_CALENDAR_ID: process.env.JRA_CALENDAR_ID ?? '',
-                NAR_CALENDAR_ID: process.env.NAR_CALENDAR_ID ?? '',
-                KEIRIN_CALENDAR_ID: process.env.KEIRIN_CALENDAR_ID ?? '',
-                WORLD_CALENDAR_ID: process.env.WORLD_CALENDAR_ID ?? '',
-                AUTORACE_CALENDAR_ID: process.env.AUTORACE_CALENDAR_ID ?? '',
-                BOATRACE_CALENDAR_ID: process.env.BOATRACE_CALENDAR_ID ?? '',
-                TEST_CALENDAR_ID: process.env.TEST_CALENDAR_ID ?? '',
-                GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL ?? '',
-                GOOGLE_PRIVATE_KEY: (
-                    process.env.GOOGLE_PRIVATE_KEY ?? ''
-                ).replace(/\\n/g, '\n'),
-            },
-            timeout: Duration.seconds(90),
-            memorySize: 1024,
-        },
+    // EFSアクセス権限をLambdaに追加
+    role.addToPolicy(
+        new PolicyStatement({
+            actions: [
+                'elasticfilesystem:ClientMount',
+                'elasticfilesystem:ClientWrite',
+            ],
+            resources: [fileSystem.fileSystemArn],
+        }),
     );
+
+    return role;
 }
