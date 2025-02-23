b87042b3a387a4699b2dc503bb66da21422a37df
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-02-23 16:43:36 +0900

    GoogleCalendarのテスト改修

--- test/src/repository/implement/narGoogleCalendarRepositoryImpl.test.ts
+++ test/src/repository/implement/narGoogleCalendarRepositoryImpl.test.ts
@@ -29,7 +29,7 @@ describe('NarGoogleCalendarRepositoryImpl', () => {
         jest.clearAllMocks();
     });
 
-    it('should fetch events successfully', async () => {
+    it('カレンダー情報が正常に取得できること', async () => {
         googleCalendarGateway.fetchCalendarDataList.mockResolvedValue([
             baseNarCalendarDataFromGoogleCalendar,
         ]);
@@ -45,7 +45,7 @@ describe('NarGoogleCalendarRepositoryImpl', () => {
         expect(googleCalendarGateway.fetchCalendarDataList).toHaveBeenCalled();
     });
 
-    it('should handle error when fetching events', async () => {
+    it('カレンダー情報が正常に取得できないこと', async () => {
         googleCalendarGateway.fetchCalendarDataList.mockRejectedValue(
             new Error('API Error'),
         );
@@ -60,7 +60,7 @@ describe('NarGoogleCalendarRepositoryImpl', () => {
         expect(googleCalendarGateway.fetchCalendarDataList).toHaveBeenCalled();
     });
 
-    it('should delete events successfully', async () => {
+    it('カレンダー情報が正常に削除できること', async () => {
         googleCalendarGateway.deleteCalendarData.mockResolvedValue();
 
         await repository.deleteEvents([baseNarCalendarData]);
@@ -68,7 +68,7 @@ describe('NarGoogleCalendarRepositoryImpl', () => {
         expect(googleCalendarGateway.deleteCalendarData).toHaveBeenCalled();
     });
 
-    it('should handle error when deleting events', async () => {
+    it('カレンダー情報が正常に削除できないこと', async () => {
         googleCalendarGateway.deleteCalendarData.mockRejectedValue(
             new Error('API Error'),
         );
@@ -77,7 +77,7 @@ describe('NarGoogleCalendarRepositoryImpl', () => {
         expect(googleCalendarGateway.deleteCalendarData).toHaveBeenCalled();
     });
 
-    it('should insert events successfully', async () => {
+    it('カレンダー情報が正常に登録できること', async () => {
         googleCalendarGateway.fetchCalendarData.mockRejectedValue(
             new Error('API Error'),
         );
@@ -87,7 +87,7 @@ describe('NarGoogleCalendarRepositoryImpl', () => {
         expect(googleCalendarGateway.insertCalendarData).toHaveBeenCalled();
     });
 
-    it('should update events successfully', async () => {
+    it('カレンダー情報が正常に更新できること', async () => {
         googleCalendarGateway.fetchCalendarData.mockResolvedValue(
             baseNarCalendarDataFromGoogleCalendar,
         );
@@ -97,7 +97,7 @@ describe('NarGoogleCalendarRepositoryImpl', () => {
         expect(googleCalendarGateway.updateCalendarData).toHaveBeenCalled();
     });
 
-    it('should handle error when upserting events', async () => {
+    it('カレンダー情報が正常に更新できないこと', async () => {
         googleCalendarGateway.insertCalendarData.mockRejectedValue(
             new Error('API Error'),
         );
